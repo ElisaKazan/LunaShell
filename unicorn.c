f0a027f3ffbbf23aec681c782ca7ef7e49b1bed8
Elisa Kazan <elisa.kazan@gmail.com>
2017-03-30 17:40:05 -0400

    Getting Started

---
+++ unicorn.c
@@ -0,0 +1,36 @@
+/*
+ * UNICORN.C
+ * <insert description>
+ */
+
+enum {
+  RUNNING = 1,
+  EXIT = 0
+} status;
+
+
+int main(int argc, char **argv) {
+  //
+
+  status = RUNNING;
+
+  // Loop
+
+  while(status == RUNNING) {
+
+    // Display prompt
+
+    // Read line
+
+    // Evaluate & Print
+
+    // Check status
+    status = RUNNING; // TODO
+
+    // Cleanup
+  
+  }
+
+  return 0;
+}
+
