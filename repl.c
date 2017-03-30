8c2e701266a82b8da6dbeb7e1e6457368e2b3ffd
Jack McCracken <jack.mccracken@shopify.com>
2017-03-30 17:41:29 -0400

    Add header files & repl.c

---
+++ repl.c
@@ -0,0 +1,18 @@
+/*
+ * REPL.C
+ *
+ * All the main REPL functionality of Unicorn Shell
+ */
+
+// read_input returns 0 on error, 1 on success.
+int read_input(char *buffer, int length, FILE *input) {
+    return 0;
+}
+
+int parse_input(char *buffer, int len, struct command* command) {
+    return 0;
+}
+
+int execute(struct command* command) {
+    return 0;
+}
