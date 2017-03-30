d44d3ec969ba1e56ae38668abec76d7bac4ad11d
elisakazan <elisa.kazan@gmail.com>
2017-03-30 18:18:32 -0400

    Fixed spacing

--- src/repl.c
+++ src/repl.c
@@ -6,15 +6,37 @@
  * All the main REPL functionality of Unicorn Shell
  */
 
-// read_input returns 0 on error, 1 on success.
-int read_input(char *buffer, int length, FILE *input) {
+/* 
+ * read_input 
+ * returns 0 on error, 1 on success.
+ */
+int read_input(char *buffer, int length, int input) {
+    
+
+    // Check length of file
+    if(1 <= length) {
+    }
+    else {
+        // Command is too long
+        // ERROR 
+    }
+
+
     return 0;
 }
 
+/* 
+ * parse_input
+ * 
+ */
 int parse_input(char *buffer, int len, command* command) {
     return 0;
 }
 
+/*
+ * execute
+ * 
+ */
 int execute(command* command) {
     return 0;
 }
--- src/unicorn.c
+++ src/unicorn.c
@@ -4,33 +4,29 @@
  */
 
 enum {
-  RUNNING = 1,
-  EXIT = 0
+    RUNNING = 1,
+    EXIT = 0
 } status;
 
 
 int main(int argc, char **argv) {
-  //
+    //
+    status = RUNNING;
 
-  status = RUNNING;
+    // Loop
+    while(status == RUNNING) {
+        // Display prompt
+        
+        // Read line
 
-  // Loop
+        // Evaluate & Print
 
-  while(status == RUNNING) {
+        // Check status
+        status = RUNNING; // TODO
 
-    // Display prompt
-
-    // Read line
-
-    // Evaluate & Print
-
-    // Check status
-    status = RUNNING; // TODO
-
-    // Cleanup
+        // Cleanup
   
-  }
-
-  return 0;
+    }
+    return 0;
 }
 
